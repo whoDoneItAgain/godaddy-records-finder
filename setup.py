bad5f88795ef8d05b0829af28f1160faede4c496
whoDoneItAgain <12127434+whoDoneItAgain@users.noreply.github.com>
2024-04-09 12:22:29 -0400

    setup updates

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ from setuptools import find_packages, setup
 def get_version(filename):
     with codecs.open(filename, "r", "utf-8") as fp:
         contents = fp.read()
-    return re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents).group(1)
+
+    v = re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents)
+
+    if v is None:
+        raise Exception(
+            "No match. Create/Update version.py to include line of __version__ = 'X.Y.Z'"
+        )
+    else:
+        return v.group(1)
 
 
 version = get_version("src/grf/version.py")
@@ -17,12 +25,12 @@ with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-    name="grf",
+    name="godaddy-records-finder",
     version=version,
-    description=("GoDaddy Record Finder"),
+    description=("GoDaddy Records Finder"),
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="whodoneitagain",
+    author="whoDoneItAgain",
     author_email="12127434+whoDoneItAgain@users.noreply.github.com",
     package_dir={"": "src"},
     packages=find_packages("src"),
@@ -32,10 +40,13 @@ setup(
     entry_points={"console_scripts": ["grf = grf.__main__:main"]},
     test_suite="unittest",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: Internet",
     ],
 )
